6b6b1aa038976a456a1a83415b1f88bf2a0d01aa
Roosterseatyou <ltbkthree@icloud.com>
2022-10-02 16:37:58 -0500

    Initial Commit

---
+++ src/main/java/xyz/roosterseatyou/elections/Elections.java
@@ -0,0 +1,16 @@
+package xyz.roosterseatyou.elections;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Elections extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
